a4d5080fca7e637df9f209ad87ca066ec8425029
Alberto Paredes <alberto.paredes.bs@gmail.com>
2020-03-23 01:07:17 -0600

    Se agregaron diccionarios y codificacion de mensajes

--- huffman.c
+++ huffman.c
@@ -1,19 +1,27 @@
-#include<stdio.h>
-#include<stdlib.h>
-void registrarSimbolo(char*, float*, int*);
-void listarSimbolos(char*, float*, int*);
-void eliminarSimbolo(char*, float*, int*);
-void modificarSimbolo(char*, float*, int*);
-void generarCodigos(char*, float*, char*);
-void cargarDiccionario(char*);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void registrarSimbolo(char *, float *, int *);
+void listarSimbolos(char *, float *, int *);
+void eliminarSimbolo(char *, float *, int *);
+void modificarSimbolo(char *, float *, int *);
+void generarCodigos(char *, float *, char *);
+void cargarDiccionario(char **, char ***, int *, int *);
+void mostrarDiccionario(char *, char **, int);
+void guardarDiccionario(char *, char **, int, int);
+void codificarMensaje(char *, char **, int, int);
+void decodificarMensaje(char *, char **, int, int);
 
-void main() {
+int main()
+{
     int opcion, tamano = 0;
     char *simbolos = NULL;
     float *frecuencias = NULL;
     char **diccionario = NULL;
-    simbolos = (char*)malloc(sizeof(char));
-    frecuencias = (float*)malloc(sizeof(float));
+    int codigoGenerado = 0;
+    simbolos = (char *)malloc(sizeof(char));
+    frecuencias = (float *)malloc(sizeof(float));
+    diccionario = (char **)malloc(sizeof(char *));
     if (simbolos == NULL)
     {
         printf("Error al reservar memoria");
@@ -24,12 +32,22 @@ void main() {
         printf("Error al reservar memoria");
         exit(0);
     }
-    
+
     do
     {
         printf("Algoritmo de Huffman para la codificacion de mensajes\n");
         printf("Seleccione una de las siguientes opciones:\n");
-        printf("1. Registrar simbolo\n2. Listar simbolos\n3. Eliminar simbolo.\n4. Modificar un simbolo.\n5. Generar codigos.\n6. Guardar diccionario (archivo).\n7. Cargar diccionario (archivo).\n8. Salir.\n");
+        printf("1. Registrar simbolo\n2. Listar simbolos\n3. Eliminar simbolo.\n4. Modificar un simbolo.\n5. Generar diccionario.\n6. Guardar diccionario (archivo).\n7. Cargar diccionario (archivo).\n");
+        if (codigoGenerado)
+        {
+            printf("8. Codificar mensaje\n9. Decodificar mensaje\n10. Salir\n");
+            mostrarDiccionario(simbolos, diccionario, tamano);
+        }
+        else
+        {
+            printf("10. Salir\n");
+        }
+
         scanf("%d", &opcion);
         system("clear");
         switch (opcion)
@@ -50,23 +68,30 @@ void main() {
             printf("En construccion");
             break;
         case 6:
-            
+            guardarDiccionario(simbolos, diccionario, tamano, codigoGenerado);
             break;
         case 7:
-            
+            cargarDiccionario(&simbolos, &diccionario, &tamano, &codigoGenerado);
             break;
         case 8:
+            codificarMensaje(simbolos, diccionario, tamano, codigoGenerado);
+            break;
+        case 9:
+            decodificarMensaje(simbolos, diccionario, tamano, codigoGenerado);
+            break;
+        case 10:
             printf("Saliendo...\n");
             break;
         default:
             printf("Seleccione una opcion valida.\n");
             break;
         }
-    } while (opcion != 8);
-    
+    } while (opcion != 10);
+    return 0;
 }
 
-void registrarSimbolo(char *simbolos, float *frecuencias, int *tamano){
+void registrarSimbolo(char *simbolos, float *frecuencias, int *tamano)
+{
     char nuevoSimbolo, *tmpSimbolos;
     float nuevaFrecuencia, *tmpFrecuencias;
 
@@ -82,52 +107,67 @@ void registrarSimbolo(char *simbolos, float *frecuencias, int *tamano){
             printf("El simbolo ya se encuentra registrado\n");
             return;
         }
-        
     }
     (*tamano)++;
-    tmpSimbolos = realloc(simbolos, sizeof(int)*(*tamano));
-    if (tmpSimbolos){
+    tmpSimbolos = realloc(simbolos, sizeof(char) * (*tamano));
+    if (tmpSimbolos)
+    {
         simbolos = tmpSimbolos;
-        simbolos[(*tamano)-1] = nuevoSimbolo;
-    }else{
+        simbolos[(*tamano) - 1] = nuevoSimbolo;
+    }
+    else
+    {
         free(tmpSimbolos);
     }
-    tmpFrecuencias = realloc(frecuencias, sizeof(float)*(*tamano));
-    if (tmpFrecuencias){
+    tmpFrecuencias = realloc(frecuencias, sizeof(float) * (*tamano));
+    if (tmpFrecuencias)
+    {
         frecuencias = tmpFrecuencias;
-        frecuencias[(*tamano)-1] = nuevaFrecuencia;
-    }else{
+        frecuencias[(*tamano) - 1] = nuevaFrecuencia;
+    }
+    else
+    {
         free(tmpFrecuencias);
     }
 }
 
-void listarSimbolos(char *simbolos, float *frecuencias, int *tamano){
-    if ((*tamano) == 0){
+void listarSimbolos(char *simbolos, float *frecuencias, int *tamano)
+{
+    if ((*tamano) == 0)
+    {
         printf("No hay simbolos registrados\n");
-    }else{
+    }
+    else
+    {
         for (int i = 0; i < (*tamano); i++)
         {
-            printf("%c - %.2f\n",simbolos[i], frecuencias[i]);
+            printf("%c - %.2f\n", simbolos[i], frecuencias[i]);
         }
     }
 }
 
-void eliminarSimbolo(char *simbolos, float *frecuencias, int *tamano){
+void eliminarSimbolo(char *simbolos, float *frecuencias, int *tamano)
+{
     int posicion_eliminar = -1;
     char simbolo, *tmpSimbolos;
     float *tmpFrecuencias;
-    if ((*tamano) == 0){
+    if ((*tamano) == 0)
+    {
         printf("No hay simbolos registrados\n");
-    }else{
+    }
+    else
+    {
         printf("Ingresa el simbolo a eliminar\n");
         scanf(" %c", &simbolo);
         for (int i = 0; i < (*tamano); i++)
         {
-            if(simbolos[i] == simbolo){
+            if (simbolos[i] == simbolo)
+            {
                 posicion_eliminar = i;
             }
         }
-        if (posicion_eliminar == -1){
+        if (posicion_eliminar == -1)
+        {
             printf("El simbolo que se quiere eliminar no se encuentra registrado\n");
             return;
         }
@@ -137,29 +177,38 @@ void eliminarSimbolo(char *simbolos, float *frecuencias, int *tamano){
             frecuencias[i] = frecuencias[i + 1];
         }
         (*tamano)--;
-        tmpSimbolos = realloc(simbolos, sizeof(int)*(*tamano));
-        if (tmpSimbolos){
+        tmpSimbolos = realloc(simbolos, sizeof(int) * (*tamano));
+        if (tmpSimbolos)
+        {
             simbolos = tmpSimbolos;
-        }else{
+        }
+        else
+        {
             free(tmpSimbolos);
         }
-        tmpFrecuencias = realloc(frecuencias, sizeof(float)*(*tamano));
-        if (tmpFrecuencias){
+        tmpFrecuencias = realloc(frecuencias, sizeof(float) * (*tamano));
+        if (tmpFrecuencias)
+        {
             frecuencias = tmpFrecuencias;
-        }else{
+        }
+        else
+        {
             free(tmpFrecuencias);
         }
     }
 }
 
-void modificarSimbolo(char *simbolos, float *frecuencias, int *tamano){
+void modificarSimbolo(char *simbolos, float *frecuencias, int *tamano)
+{
     int posicionModificar = -1;
     char simbolo;
     float nuevaFrecuencia;
     if ((*tamano) == 0)
     {
         printf("No hay simbolos registrados\n");
-    }else{
+    }
+    else
+    {
         printf("Ingresa el simbolo a modificar\n");
         scanf(" %c", &simbolo);
         printf("Ingresa el nuevo valor de frecuencia para %c\n", simbolo);
@@ -171,13 +220,227 @@ void modificarSimbolo(char *simbolos, float *frecuencias, int *tamano){
             {
                 posicionModificar = i;
             }
-            
         }
-        if (posicionModificar == -1){
+        if (posicionModificar == -1)
+        {
             printf("No se encontro el simbolo a modificar\n");
-        }else{
+        }
+        else
+        {
             frecuencias[posicionModificar] = nuevaFrecuencia;
         }
     }
-    
+}
+
+void mostrarDiccionario(char *simbolos, char **diccionario, int tamano)
+{
+    for (int i = 0; i < tamano; i++)
+    {
+        printf("%c %s %d\n", simbolos[i], diccionario[i], strlen(diccionario[i]));
+    }
+}
+
+void cargarDiccionario(char **simbolos, char ***diccionario, int *tamano, int *codigoGenerado)
+{
+    char opcion, buffer[128], nombreArchivo[30], *codigo;
+    if ((*tamano) > 0)
+    {
+        printf("Los simbolos registrados anteriormente seran borrados Desea continuar? [Y/n]\n");
+        scanf(" %c", &opcion);
+        if (opcion == 'n')
+        {
+            return;
+        }
+        else if (opcion == 'Y' || opcion == 'y')
+        {
+            for (int i = 0; i < (*tamano); i++)
+            {
+                free((*diccionario)[i]);
+            }
+            free(*diccionario);
+            free(*simbolos);
+            *diccionario = NULL;
+            *(tamano) = 0;
+        }
+    }
+    printf("Introduzca el nombre del archivo que contiene el diccionario\n");
+    scanf("%s", nombreArchivo);
+    FILE *archivo = fopen(nombreArchivo, "r");
+    if (archivo == NULL)
+    {
+        printf("Error abriendo el archivo\n");
+        return;
+    }
+    int i = 0;
+    int j = 0;
+    int tamanoCodigo = 0;
+    char *tmpSimbolos, *tmpCodigo, **tmpDiccionario;
+    codigo = (char *)malloc(sizeof(char));
+    *simbolos = (char *)malloc(sizeof(char));
+    *diccionario = (char **)malloc(sizeof(char *));
+    while (fgets(buffer, sizeof(buffer), archivo) != NULL)
+    {
+        j = 0;
+        tamanoCodigo = 0;
+        printf("%s %ld", buffer, strlen(buffer));
+        while (buffer[j] != '\n')
+        {
+            if (j == 0)
+            {
+                (*tamano)++;
+                tmpSimbolos = (char *)realloc(*simbolos, sizeof(char) * (*tamano));
+                if (tmpSimbolos)
+                {
+                    *simbolos = tmpSimbolos;
+                    (*simbolos)[(*tamano) - 1] = buffer[j];
+                }
+                else
+                {
+                    free(tmpSimbolos);
+                }
+            }
+            else if (j != 1)
+            {
+                tamanoCodigo++;
+                tmpCodigo = (char *)realloc(codigo, sizeof(char) * tamanoCodigo);
+                if (tmpCodigo)
+                {
+                    codigo = tmpCodigo;
+                    codigo[tamanoCodigo - 1] = buffer[j];
+                }
+                else
+                {
+                    free(tmpCodigo);
+                }
+            }
+            // printf("%c", buffer[j]);
+            j++;
+        }
+        tamanoCodigo++;
+        tmpCodigo = (char *)realloc(codigo, sizeof(char) * tamanoCodigo);
+        if (tmpCodigo)
+        {
+            codigo = tmpCodigo;
+            codigo[tamanoCodigo - 1] = '\0';
+        }
+        else
+        {
+            free(tmpCodigo);
+        }
+        tmpDiccionario = (char **)realloc(*diccionario, sizeof(char *) * (*tamano));
+        if (tmpDiccionario)
+        {
+            *diccionario = tmpDiccionario;
+            (*diccionario)[(*tamano) - 1] = (char *)malloc(sizeof(char) * tamanoCodigo);
+            strcpy((*diccionario)[(*tamano)-1], codigo);
+        }
+        else
+        {
+            free(tmpDiccionario);
+        }
+        i++;
+    }
+    fclose(archivo);
+    (*codigoGenerado) = 1;
+}
+
+void guardarDiccionario(char *simbolos, char **diccionario, int tamano, int codigoGenerado){
+    if (codigoGenerado == 1)
+    {
+        FILE *archivo = fopen("diccionarioGuardado.txt", "w");
+        for (int i = 0; i < tamano; i++)
+        {
+            fprintf(archivo,"%c-%s\n", simbolos[i], diccionario[i]);
+        }
+        printf("Archivo guardado como: diccionarioGuardado.txt\n");
+        fclose(archivo);
+    }
+    else
+    {
+        printf("No hay ningun diccionario generado\n");
+    }
+}
+
+void codificarMensaje(char *simbolos, char **diccionario, int tamano, int codigoGenerado){
+    char nombreArchivo[30], caracter;
+    int j = 0;
+    if(codigoGenerado == 1){
+        printf("Introduzca el nombre del archivo que contiene el mensaje\n");
+        scanf("%s", nombreArchivo);
+        FILE *archivo = fopen(nombreArchivo, "r");
+        FILE *archivoCodificado = fopen("mensajeCodificado.txt", "w");
+        if (archivo == NULL || archivoCodificado == NULL)
+        {
+            printf("Error abriendo el archivo\n");
+            return;
+        }
+        while ((caracter = getc(archivo)) != EOF)
+        {
+            for (int i = 0; i < tamano; i++)
+            {
+                if (simbolos[i] == caracter)
+                {
+                    j = 0;
+                    while (diccionario[i][j] == '0' || diccionario[i][j] == '1')
+                    {
+                        fputc(diccionario[i][j],archivoCodificado);
+                        j++;
+                    }
+                    break;
+                }
+            }
+        }
+        printf("Mensaje codificado guardado en: mensajeCodificado.txt\n");
+        fclose(archivo);
+        fclose(archivoCodificado);
+    }
+}
+
+void decodificarMensaje(char *simbolos, char **diccionario, int tamano, int codigoGenerado){
+    char nombreArchivo[30], caracter;
+    char *codigo = (char*)malloc(sizeof(char)), *tmpCodigo;
+    int tamanoCodigo = 1;
+    if (codigoGenerado == 1)
+    {
+        printf("Introduzca el nombre del archivo que contiene el mensaje codificado\n");
+        scanf("%s", nombreArchivo);
+        FILE *archivo = fopen(nombreArchivo, "r");
+        FILE *archivoDecodificado = fopen("mensajeDecodificado.txt", "w");
+        if (archivo == NULL || archivoDecodificado == NULL)
+        {
+            printf("Error abriendo el archivo\n");
+            return;
+        }
+        while ((caracter = getc(archivo)) != EOF)
+        {
+            tamanoCodigo++;
+            tmpCodigo = (char*)realloc(codigo, sizeof(char)*tamanoCodigo);
+            if (tmpCodigo)
+            {
+                codigo = tmpCodigo;
+                codigo[tamanoCodigo-2] = '\0';
+                codigo[tamanoCodigo-1] = caracter;
+                // printf("%s\n", codigo);
+                for (int i = 0; i < tamano; i++)
+                {
+                    printf("%ld - %ld\n",strlen(codigo), strlen(diccionario[i]));
+                    printf("%s - %s.\n",codigo, diccionario[i]);
+                    // if (strcmp(codigo, diccionario[i]) == 0)
+                    // {
+                    //     printf("*");
+                    //     fputc(simbolos[i], archivoDecodificado);
+                    //     tamanoCodigo = 0;
+                    // }
+                }
+                break;
+            }
+            else
+            {
+                free(tmpCodigo);
+            }
+            
+        }
+        fclose(archivo);
+        fclose(archivoDecodificado);
+    }
 }
